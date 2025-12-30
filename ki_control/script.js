f4ba1ad6719e2a89bc4e73d155b8053800b9ead8
AlejandroSanchezSerrano <alexxsanse@gmail.com>
2025-12-30 14:03:35 +0100

    Se sube la pagina ki control y se prueba si se puede acceder

--- ki_control/script.js
+++ ki_control/script.js
@@ -1,6 +1,5 @@
 // --- CONFIGURACIÓN ---
-// URL donde subiste los PHP (ej: https://tudominio.com/api). Sin barra al final.
-const API_BASE_URL = 'https://188.164.199.21/api'; 
+const API_BASE_URL = 'https://alejandrodev.es/api'; 
 
 const year = 2026;
 let currentMonth = 0; 
